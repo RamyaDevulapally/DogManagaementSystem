613d027e5e0f62575c909cf0c466e3cf47899504
Ramya Devulapally <S547918@nwmissouri.edu>
2023-04-29 21:40:06 -0500

    dogsmgt

---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.exampled.DMS.models.Trainer;
+
+/**
+ * @author S547918
+ *
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer>  {
+
+}
